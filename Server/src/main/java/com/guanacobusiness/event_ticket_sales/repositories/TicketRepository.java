17fd3ab108545648a96fe341c17003136ab35ccd
joshua1535 <00176020@uca.edu.sv>
2023-06-04 02:04:11 -0600

    Create Ticket Repository and Service
    Creating Ticket Repository and Service for the Ticket Entity.

---
+++ Server/src/main/java/com/guanacobusiness/event_ticket_sales/repositories/TicketRepository.java
@@ -0,0 +1,11 @@
+package com.guanacobusiness.event_ticket_sales.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+import com.guanacobusiness.event_ticket_sales.models.entities.Ticket;
+
+public interface TicketRepository extends ListCrudRepository<Ticket, UUID>{
+    
+}
---
+++ Server/src/main/java/com/guanacobusiness/event_ticket_sales/services/implementations/TicketServiceImpl.java
@@ -0,0 +1,62 @@
+package com.guanacobusiness.event_ticket_sales.services.implementations;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guanacobusiness.event_ticket_sales.models.dtos.ChangeOwnershipDTO;
+import com.guanacobusiness.event_ticket_sales.models.dtos.SaveTicketDTO;
+import com.guanacobusiness.event_ticket_sales.models.dtos.ValidateTicketDTO;
+import com.guanacobusiness.event_ticket_sales.models.entities.Ticket;
+import com.guanacobusiness.event_ticket_sales.models.entities.User;
+import com.guanacobusiness.event_ticket_sales.repositories.TicketRepository;
+import com.guanacobusiness.event_ticket_sales.repositories.UserRepository;
+import com.guanacobusiness.event_ticket_sales.services.TicketService;
+
+@Service
+public class TicketServiceImpl implements TicketService{
+    
+    @Autowired
+    TicketRepository ticketRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public void save(SaveTicketDTO saveTicketDTO) throws Exception {
+        Ticket ticket = new Ticket(saveTicketDTO.getOrder(), saveTicketDTO.getTier(), saveTicketDTO.getUserOwner());
+
+        ticketRepository.save(ticket);
+
+    }
+
+    @Override
+    public Boolean changeOwnership(ChangeOwnershipDTO changeOwnershipDTO) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'changeOwnership'");
+    }
+
+    @Override
+    public List<Ticket> findAllTickets(UUID userOwnerCode) {
+    
+        User user = userRepository.findByCode(userOwnerCode);
+
+        if(user == null) {
+            return null;
+        }
+
+        List<Ticket> tickets = user.getTickets();
+
+        return tickets;
+    
+    }
+
+    @Override
+    public Boolean validateTicket(ValidateTicketDTO validateTicketDTO) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validateTicket'");
+    }
+
+}
