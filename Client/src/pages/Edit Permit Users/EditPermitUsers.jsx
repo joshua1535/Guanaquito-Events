664a66439b97978935883466ecef2e4ca3c804f7
WilmerSorto19 <62612059+00146520@users.noreply.github.com>
2023-11-21 17:10:11 -0600

    Revert "Fix design epu"

--- Client/src/pages/Edit Permit Users/EditPermitUsers.jsx
+++ Client/src/pages/Edit Permit Users/EditPermitUsers.jsx
@@ -553,13 +553,12 @@ useEffect(() => {
               >
                 <div className="flex justify-center ">
                 <Avatar
-                  src={userfiltred.profilePicture}
+                  src="https://s3.amazonaws.com/moonup/production/uploads/1670331935393-6312579fc7577b68d90a7646.png"
                   
                   className="w-auto h-auto 
-                            PC-1920*1080:w-screen
-                            PC-1600*900:w-2/4 
-                            PC-1280*720:w-44 
-                            md:w-1/2 lg:w-1/3"
+                  PC-1920*1080:w-screen
+                  PC-1600*900:w-2/4 
+                  PC-1280*720:w-44 "
                 />
                 </div>
                 <div className={classes["emailContainer"]}>
@@ -571,6 +570,8 @@ useEffect(() => {
           </div>
 
           <div className={classes["listPermitsContainer"]}>
+          <div className={classes["permitListContainer"]}>
+          <div className="flex flex-col w-full items-center justify-center" >
           <div 
               className={`p-2 m-2 flex w-full items-center justify-start "}`}
             >
@@ -580,17 +581,14 @@ useEffect(() => {
                 PC-1280*720:text-2xl PC-1280*720:w-full PC-1280*720:m-1">
                 Permisos
               </Typography>              
-            </div>
-          <div className={classes["permitListContainer"]}>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:mr-12 sm:mx-auto">
-           
+            </div> 
             
             <div 
               style={{ cursor: "pointer" }}
-              className={`p-2 m-2 w-32 rounded-md h-20 flex items-center justify-center ${permisoCliente ? "border-4 border-green-500 " : "border-4 opacity-40 border-red-500 "}`}
+              className={`p-2 m-2 w-1/2 flex items-center justify-center ${permisoCliente ? "border-4 border-green-500" : "border-4 opacity-40 border-red-500"}`}
               onClick={handleClienteClick}
             >
-              <Typography className="m-2 m-4  text-yellowCapas 
+              <Typography className="ml-2 m-4 font-bold text-yellowCapas 
                 PC-1920*1080:text-2xl 
                 PC-1600*900:text-base PC-1600*900:m-1 
                 PC-1280*720:text-sm PC-1280*720:m-1">
@@ -600,7 +598,7 @@ useEffect(() => {
 
             <div 
               style={{ cursor: "pointer" }}
-              className={`p-2 m-2 w-32 rounded-md h-20 flex items-center justify-center ${permisoVerEstadisticas ? "border-4 border-green-500" : "border-4 opacity-40 border-red-500"}`}
+              className={`p-2 m-2 w-1/2 flex items-center justify-center ${permisoVerEstadisticas ? "border-4 border-green-500" : "border-4 opacity-40 border-red-500"}`}
               onClick={handleVerEstadisticasClick}
             >
               <Typography className="ml-2 m-4 font-bold text-yellowCapas 
@@ -613,7 +611,7 @@ useEffect(() => {
 
               <div 
               style={{ cursor: "pointer" }}
-              className={`p-2 m-2 flex w-32 rounded-md h-20 items-center justify-center whitespace-normal ${permisoValidarTickets ? "border-4 border-green-500" : "border-4 opacity-40 border-red-500"}`}
+              className={`p-2 m-2 flex w-1/2 items-center justify-center ${permisoValidarTickets ? "border-4 border-green-500" : "border-4 opacity-40 border-red-500"}`}
               onClick={handleValidarTicketsClick}
             >
               <Typography className="ml-2 m-4 font-bold text-yellowCapas 
@@ -628,7 +626,7 @@ useEffect(() => {
 
               <div 
               style={{ cursor: "pointer" }}
-              className={`p-2 m-2  flex w-32 rounded-md h-20 items-center justify-center whitespace-normal  ${permisoAdministrarEventos ? "border-4 border-green-500" : "border-4 opacity-40 border-red-500"}`}
+              className={`p-2 m-2 flex w-1/2 items-center justify-center ${permisoAdministrarEventos ? "border-4 border-green-500" : "border-4 opacity-40 border-red-500"}`}
               onClick={handleAdministrarEventosClick}
             >
               <Typography className="ml-2 m-4 font-bold text-yellowCapas 
@@ -643,7 +641,7 @@ useEffect(() => {
 
               <div 
               style={{ cursor: "pointer" }}
-              className={`p-2 m-2 flex w-32 rounded-md h-20 items-center justify-center ${permisoModerador ? "border-4 border-green-500" : "border-4 opacity-40 border-red-500"}`}
+              className={`p-2 m-2 flex w-1/2 items-center justify-center ${permisoModerador ? "border-4 border-green-500" : "border-4 opacity-40 border-red-500"}`}
               onClick={handleModeradorClick}
             >
               <Typography className="ml-2 m-4 font-bold text-yellowCapas 
@@ -658,19 +656,18 @@ useEffect(() => {
 
               <div 
               style={{ cursor: "pointer" }}
-              className={`p-2 m-2 flex w-32 rounded-md h-20 items-center justify-center whitespace-normal ${permisoAdmin ? "border-4 border-green-500" : "border-4 opacity-40 border-red-500"}`}
+              className={`p-2 m-2 flex w-1/2 items-center justify-center ${permisoAdmin ? "border-4 border-green-500" : "border-4 opacity-40 border-red-500"}`}
               onClick={handleAdminClick}
             >
               <Typography className="ml-2 m-4 font-bold text-yellowCapas 
                 PC-1920*1080:text-2xl 
                 PC-1600*900:text-base PC-1600*900:m-1 
                 PC-1280*720:text-sm PC-1280*720:m-1">
-                Super Administrador
+                Super Admin
               </Typography>
 
               
             </div>
-            </div> 
             <div className={classes["buttonsContainer"]}>
             <div className={classes["buttonContainer"]}>
                 <Button 
@@ -688,7 +685,7 @@ useEffect(() => {
             </div>
             </div>    
 
-             
+            </div>  
         </div>
 
           </div>
