3f620adeb6cedd8e2118437454e80b555320bd2f
ManuelMadriz <00026220@gmail.com>
2023-11-23 14:16:42 -0600

    refactor: replace header in all pages with Header component

--- Client/src/pages/Modify Event/ModifyEvent.jsx
+++ Client/src/pages/Modify Event/ModifyEvent.jsx
@@ -34,7 +34,7 @@ import { useUserContext } from '../../Context/userContext';
 import { eventService } from '../../Services/eventService';
 import { tierService } from '../../Services/tierService';
 import Footer from '../../Components/Footer';
-
+import Header from '../../Components/Header/Header';
 
 
   const categoryOptions = [
@@ -44,87 +44,6 @@ import Footer from '../../Components/Footer';
     "Música",
     // Añade más categorías según tus necesidades
   ];
-
-// profile menu component
-const profileMenuItems = [
-  {
-    label: "Gestionar eventos",
-  },
-  {
-    label: "Crear evento",
-  },
-  {
-    label: "Sign Out",
-  },
-];
- 
-function ProfileMenu() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  const closeMenu = () => setIsMenuOpen(false);
-
-  const navigate = useNavigate();
-
-  const handleMenuClick = (label) => {
-  if (label === "Gestionar eventos") {
-      navigate('/admin-event');
-  } else if (label === "Crear evento") {
-      navigate('/admin-event/createevent');
-  } else if (label === "Sign Out") {
-      navigate('/');
-  }
-  };
- 
-  return (
-    <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
-      <MenuHandler>
-        <Button
-          variant="text"
-          color="blue-gray"
-          className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 ml-auto"
-        >
-          <Avatar
-            variant="circular"
-            size="sm"
-            alt="candice wu"
-            className="border border-blue-500 p-0.5"
-            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-          />
-          <ChevronDownIcon
-            strokeWidth={2.5}
-            className={`h-3 w-3 transition-transform ${
-              isMenuOpen ? "rotate-180" : ""
-            }`}
-          />
-        </Button>
-      </MenuHandler>
-      <MenuList className="p-1">
-        {profileMenuItems.map(({ label }, key) => {
-          const isLastItem = key === profileMenuItems.length - 1;
-          return (
-            <MenuItem
-              key={label}
-              onClick={ () => handleMenuClick(label)}
-              className={`flex items-center gap-2 rounded ${
-                isLastItem
-                  ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
-                  : ""
-              }`}
-            >
-              <Typography
-                as="span"
-                className="font-normal"
-                color={isLastItem ? "red" : "inherit"}
-              >
-                {label}
-              </Typography>
-            </MenuItem>
-          );
-        })}
-      </MenuList>
-    </Menu>
-  );
-}
-
 
 export default function ModifyEvent() {
     const [isNavOpen, setIsNavOpen] = React.useState(false);
@@ -370,22 +289,7 @@ export default function ModifyEvent() {
 
     return (
         <div className={[classes["generalContainer"]]}>
-        <header className={[classes["headerContainer"]]}>
-      <Navbar className="sticky inset-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4 bg-dark-blue border-none">
-      <div className={[classes["headerTypography"]]}>
-        <img src={logo} alt="logo" className="h-12 w-12 mx-4" />
-        <Typography
-          as="a"
-          href="#"
-          className="mr-4 ml-2 cursor-pointer py-1.5 font-medium text-white"
-        >
-          Guanaco Business
-        </Typography>
-        
-        <ProfileMenu />
-        </div>
-    </Navbar>
-      </header>
+        <Header/>
       <div className={[classes["buttonchangeView"]]}>
       <button
       onClick={handleButtonClick2} 
--- Client/src/pages/Ticket Validation/TicketValidation.jsx
+++ Client/src/pages/Ticket Validation/TicketValidation.jsx
@@ -23,87 +23,7 @@ import { useNavigate } from 'react-router-dom';
 import { QrReader } from 'react-qr-reader';
 import { registerService } from '../../Services/registerService';
 import { useUserContext } from '../../Context/userContext';
-
-//profile menu component
-const profileMenuItems = [
-  {
-    label: "Gestionar eventos",
-  },
-  {
-    label: "Validar QR",
-  },
-  {
-    label: "Sign Out",
-  },
-];
- 
-function ProfileMenu() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  const closeMenu = () => setIsMenuOpen(false);
-
-  const navigate = useNavigate();
-
-  const handleMenuClick = (label) => {
-  if (label === "Gestionar eventos") {
-      navigate('/admin-graphs');
-  } else if (label === "Validar QR") {
-      navigate('/admin-scanner');
-  } else if (label === "Sign Out") {
-      navigate('/');
-  }
-  };
- 
-  return (
-    <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
-      <MenuHandler>
-        <Button
-          variant="text"
-          color="blue-gray"
-          className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 ml-auto"
-        >
-          <Avatar
-            variant="circular"
-            size="sm"
-            alt="candice wu"
-            className="border border-blue-500 p-0.5"
-            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-          />
-          <ChevronDownIcon
-            strokeWidth={2.5}
-            className={`h-3 w-3 transition-transform ${
-              isMenuOpen ? "rotate-180" : ""
-            }`}
-          />
-        </Button>
-      </MenuHandler>
-      <MenuList className="p-1">
-        {profileMenuItems.map(({ label }, key) => {
-          const isLastItem = key === profileMenuItems.length - 1;
-          return (
-            <MenuItem
-              key={label}
-              onClick={ () => handleMenuClick(label)}
-              className={`flex items-center gap-2 rounded ${
-                isLastItem
-                  ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
-                  : ""
-              }`}
-            >
-              <Typography
-                as="span"
-                className="font-normal"
-                color={isLastItem ? "red" : "inherit"}
-              >
-                {label}
-              </Typography>
-            </MenuItem>
-          );
-        })}
-      </MenuList>
-    </Menu>
-  );
-}
-  
+import Header from '../../Components/Header/Header';
 
   const TicketValidationPage = () => {
     const [isNavOpen, setIsNavOpen] = React.useState(false);
@@ -154,22 +74,7 @@ function ProfileMenu() {
 
     return (
         <>
-        <header className={[classes["headerContainer"]]}>
-        <Navbar className="sticky inset-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4 bg-dark-blue border-none">
-        <div className={[classes["headerTypography"]]}>
-          <img src={logo} alt="logo" className="h-12 w-12 mx-4" />
-          <Typography
-            as="a"
-            href="#"
-            className="mr-4 ml-2 cursor-pointer py-1.5 font-medium text-white"
-          >
-            Guanaco Business
-          </Typography>
-          
-          <ProfileMenu />
-          </div>
-        </Navbar>
-        </header>
+        <Header/>
     <div className="flex flex-col items-center px-4 sm:px-0 ">
                 <div className='w-1/3 
                 PC-1920*1080:w-1/3
