29fe524f15e3e671c4f438e511ddb9cd094e11d8
Jonathan Morales <00095520@uca.edu.sv>
2024-07-05 18:54:08 -0600

    Adding Department to EventLocations

--- Server/src/main/java/com/guanacobusiness/event_ticket_sales/services/EventLocationService.java
+++ Server/src/main/java/com/guanacobusiness/event_ticket_sales/services/EventLocationService.java
@@ -1,9 +1,12 @@
 package com.guanacobusiness.event_ticket_sales.services;
 
+import java.util.UUID;
 import java.util.List;
 
 import com.guanacobusiness.event_ticket_sales.models.dtos.EventLocationDTO;
 
 public interface EventLocationService {
     List<EventLocationDTO>findAllEventLocations();
+    EventLocationDTO findEventLocationByCode(UUID code);
+    List<EventLocationDTO> findEventLocationsByDepartment(String code);
 }
