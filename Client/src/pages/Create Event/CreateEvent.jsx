7b2a46f878d0c41856acb44387aba2383be2c3d0
Wilmer Hernandez <00146520@uca.edu.sv>
2024-07-06 17:17:05 -0600

    feature: recommended events have been added according to the API

--- Client/src/pages/Create Event/CreateEvent.jsx
+++ Client/src/pages/Create Event/CreateEvent.jsx
@@ -391,10 +391,10 @@ export default function CreateEvent() {
                 className='text-black bg-white '
                 onChange={value => setCategory(value)}
                 >
-                    <Option value="CI" >Cine</Option>
-                    <Option value="MU"> Musica</Option>
-                    <Option value="OB">Obras de teatro</Option>
-                    <Option value="DE">Deportes</Option>
+                    <Option value="CINE" >Cine</Option>
+                    <Option value="MUSC"> Musica</Option>
+                    <Option value="OBTR">Obras de teatro</Option>
+                    <Option value="DEPO">Deportes</Option>
                 </Select>
             </div>
             <div>
--- Client/src/pages/EventsPage/EventsPage.jsx
+++ Client/src/pages/EventsPage/EventsPage.jsx
@@ -53,6 +53,7 @@ const EventsPage = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
   const toggleIsNavOpen = () => setIsNavOpen((cur) => !cur);
   const [events, setEvents] = useState([]);
+  const [recommendedEvents, setRecommendedEvents] = useState([]);
   const { user, token } = useUserContext();
   const navigate = useNavigate();
 
@@ -64,6 +65,14 @@ const EventsPage = () => {
         })
         .catch((error) => {
           console.error('Hubo un error al obtener los eventos:', error);
+        });
+
+      eventService.getRecommendedEvents(token)
+        .then((data) => {
+          setRecommendedEvents(data);
+        })
+        .catch((error) => {
+          console.error('Hubo un error al obtener las recomendaciones:', error);
         });
     }
   }, [token]);
@@ -133,6 +142,39 @@ const EventsPage = () => {
               </Marker>
             ))}
           </MapContainer>
+        </div>
+      </div>
+
+      <div className={classes["eventsTitleList"]}>
+        <h1>Nuestras recomendaciones para ti</h1>
+      </div>
+      <div className="flex justify-center sm:flex-row h-screen bg-dark-blue mt-1 mb-2">
+        
+        <div className="w-full bg-dark-blue sm:w-3/4 p-4 ">
+          <div className="flex p-0 flex-wrap sm:space-x-4 justify-center">
+            {events.filter(event => selectedCategory === "Todos" || event.category.code === selectedCategory).map((event, index) => (
+              <div className="p-4 rounded-lg m-2 sm:m-0" key={index}>
+                <div className="w-40 h-56 overflow-hidden relative">
+                  <img
+                    src={event.image}
+                    alt="Imagen de evento"
+                    className="w-full h-full object-cover mb-2 rounded transform transition-all duration-300 hover:opacity-5"
+                  />
+                  <div style={{ fontFamily: "PoppinsLight" }} className="absolute inset-0 flex flex-col items-center justify-center opacity-0 bg-black bg-opacity-70 text-Orange font-bold transition-all duration-300 hover:opacity-100">
+                    <p className="text-xl">{event.title}</p>
+                    <p className="text-lg">{event.date}</p>
+                  </div>
+                </div>
+                <button
+                  onClick={() => navigate(`/buytickets/${event.code}`)}
+                  className="bg-Orange text-white px-4 py-2 rounded hover:bg-orange-600 hover:text-dark-blue active:scale-90 transition-all duration-150"
+                  style={{ fontFamily: "PoppinsLight" }}
+                >
+                  Comprar boleto
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
 
